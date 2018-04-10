7cf44325191029fc080ff01091978baaa705456e
Sven Kolarić <sven.kolaric@gmail.com>
2018-04-10 15:27:13 +0200

    Reverted, it didnt work

--- CS_WebApi_Cass_Docker/DAL/Program.cs
+++ CS_WebApi_Cass_Docker/DAL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Cassandra;
 
 namespace DAL
 {
@@ -6,7 +7,24 @@ namespace DAL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Builder cassandraBuilder = Cluster.Builder();
+            cassandraBuilder.AddContactPoint("127.0.0.1");
+
+            var cluster = cassandraBuilder.Build();
+
+            ISession session = cluster.Connect("webapicassdb");
+
+            RowSet rowSet = session.Execute("select * from users");
+
+
+            //foreach (var row in rowSet)
+            //{
+            //    foreach (var roww in row)
+            //        Console.Write(roww.ToString() + ' ');
+            //    Console.WriteLine();
+            //}
+
+            //Console.Read();
         }
     }
 }
