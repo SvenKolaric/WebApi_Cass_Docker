b3f493fd6100ff9c4e97e26b221b7fd40d260d07
Sven Kolarić <sven.kolaric@gmail.com>
2018-04-10 15:21:04 +0200

    Revert "Added nugets, trying out OWIN"
    This reverts commit ea6266bf2c91dd8e1731a6a07059a020721ef906.

--- CS_WebApi_Cass_Docker/DAL/Program.cs
+++ CS_WebApi_Cass_Docker/DAL/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Cassandra;
 
 namespace DAL
 {
@@ -7,24 +6,7 @@ namespace DAL
     {
         static void Main(string[] args)
         {
-            Builder cassandraBuilder = Cluster.Builder();
-            cassandraBuilder.AddContactPoint("127.0.0.1");
-
-            var cluster = cassandraBuilder.Build();
-
-            ISession session = cluster.Connect("webapicassdb");
-
-            RowSet rowSet = session.Execute("select * from users");
-
-
-            //foreach (var row in rowSet)
-            //{
-            //    foreach (var roww in row)
-            //        Console.Write(roww.ToString() + ' ');
-            //    Console.WriteLine();
-            //}
-
-            //Console.Read();
+            Console.WriteLine("Hello World!");
         }
     }
 }
