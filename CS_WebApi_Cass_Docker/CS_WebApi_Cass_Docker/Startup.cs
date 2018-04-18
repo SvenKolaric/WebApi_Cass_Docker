d25c1f9094d15fde09ec95f86a980edb77a2e409
Sven Kolarić <sven.kolaric@gmail.com>
2018-04-18 13:02:55 +0200

    Implementing JWT tokens, changed startup class and appsettings.json

--- CS_WebApi_Cass_Docker/CS_WebApi_Cass_Docker/Startup.cs
+++ CS_WebApi_Cass_Docker/CS_WebApi_Cass_Docker/Startup.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CS_WebApi_Cass_Docker
 {
@@ -23,7 +27,30 @@ namespace CS_WebApi_Cass_Docker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //Register JWT authentication schema, Jwt:Issuer and Jwt:Key stored in appsettings.json
+            //For the JWT to be valid:
+            //1.Validate that the server created that token (ValidateIssuer = true)
+            //2.Recipient is authorized to receive it (ValidateAudience = true)
+            //3.Check expiration date and the signing key of the issuer (ValidateLifetime = true)
+            //4.Verify signing key is part of a list of trusted keys (ValidateIssuerSigningKey = true)
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+                AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +60,8 @@ namespace CS_WebApi_Cass_Docker
             {
                 app.UseDeveloperExceptionPage();
             }
+            //Use your tokens
+            app.UseAuthentication();
 
             app.UseMvc();
         }
